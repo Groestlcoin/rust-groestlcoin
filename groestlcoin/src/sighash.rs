557967a96d5fc64fdca64a02572fca968900aaef
HashEngineering <hashengineeringsolutions@gmail.com>
2022-12-22 09:20:57 -0800

    fix: convert properly from Hash to Message

--- groestlcoin/src/sighash.rs
+++ groestlcoin/src/sighash.rs
@@ -1549,7 +1549,7 @@ mod tests {
                 .unwrap();
 
             //let msg = secp256k1::Message::from(sighash);
-            let msg = secp256k1::Message::from_slice(&sighash).unwrap();
+            let msg = secp256k1::Message::from_slice(sighash.as_ref()).unwrap();
             let key_spend_sig = secp.sign_schnorr_with_aux_rand(&msg, &tweaked_keypair, &[0u8; 32]);
 
             assert_eq!(expected.internal_pubkey, internal_key);
